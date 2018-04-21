17422482a0c1f8d11cc36e21e70f4fdca40102b0
Songyu Wang <wangs4@rose-hulman.edu>
2018-04-20 21:11:55 -0400

    fix

--- FE_server.py
+++ FE_server.py
@@ -1,10 +1,11 @@
 import redis
-import os
-from google.cloud import bigtable
-from google.cloud import happybase
 from csv import reader
+import pickle
+pickle.HIGHEST_PROTOCOL = 2
 from rq import Queue
 from time import sleep
+from bigtable_funcs import *
+
 ##############connection setup####################################
 
 cache_conn = redis.StrictRedis(host='433-17.csse.rose-hulman.edu', port=6379, db=0)
@@ -30,80 +31,6 @@ def dataflow_test(_):
 
 def cache_flushall(_):
     return cache_conn.flushall()
-
-
-############bigtable function##########################
-
-def bigtable_test1():
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "key.json"
-    project_id= "csse433-adb-201117"
-    instance_id ="project433"
-    table_name = 'test'
-
-    client = bigtable.Client(project=project_id, admin=True)
-    instance = client.instance(instance_id)
-    connection = happybase.Connection(instance=instance)
-
-    print('Creating the {} table.'.format(table_name))
-    column_family_name = 'test_cf'
-    connection.create_table(
-        table_name,
-        {
-            column_family_name: dict()  # Use default options.
-        })
-
-    print('Writing some greetings to the table.')
-    table = connection.table(table_name)
-    column_name = '{fam}:test_c'.format(fam=column_family_name)
-    row = [
-        'bigtable_info'
-    ]
-
-    for i, value in enumerate(row):
-        row_key = 'test_c{}'.format(i)
-        table.put(row_key, {column_name: value})
-    connection.close()
-
-def bigtable_test2():
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "key.json"
-    project_id = "csse433-adb-201117"
-    instance_id = "project433"
-    table_name = 'test'
-
-    client = bigtable.Client(project=project_id, admin=True)
-    instance = client.instance(instance_id)
-    connection = happybase.Connection(instance=instance)
-    table = connection.table(table_name)
-    column_family_name = 'test_cf'
-    column_name = '{fam}:test_c'.format(fam=column_family_name)
-    print('Getting a single greeting by row key.')
-    key = 'test_c0'.encode('utf-8')
-    row = table.row(key)
-    print('\t{}: {}'.format(key, row[column_name.encode('utf-8')]))
-    connection.close()
-
-def bigtable_test3():
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "key.json"
-    project_id = "csse433-adb-201117"
-    instance_id = "project433"
-    table_name = 'test'
-
-    client = bigtable.Client(project=project_id, admin=True)
-    instance = client.instance(instance_id)
-    connection = happybase.Connection(instance=instance)
-    print('Deleting the {} table.'.format(table_name))
-    connection.delete_table(table_name)
-
-    connection.close()
-
-
-
-
-
-
-
-
-
 
 
 ###############main############################
