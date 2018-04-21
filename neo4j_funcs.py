f47e6d4b13920015f4d3366e8a0eabc989ff2ff4
Songyu Wang <wangs4@rose-hulman.edu>
2018-04-20 22:08:09 -0400

    fix

--- neo4j_funcs.py
+++ neo4j_funcs.py
@@ -1,7 +1,7 @@
 from neo4jrestclient.client import GraphDatabase
 from neo4jrestclient import client
 def neo4j_test1():
-    db = GraphDatabase("bolt://433-18.csse.rose-hulman.edu:7687", username="neo4j",password="csse433")
+    db = GraphDatabase("http://433-18.csse.rose-hulman.edu:7687", username="neo4j",password="csse433")
     db.labels.create("Test")
     test = db.nodes.create(name="test")
 
@@ -11,14 +11,14 @@ def neo4j_test1():
     test.relationships.create("tt", test2)
 
 def neo4j_test2():
-    db = GraphDatabase("bolt://433-18.csse.rose-hulman.edu:7687", username="neo4j", password="csse433")
+    db = GraphDatabase("http://433-18.csse.rose-hulman.edu:7687", username="neo4j", password="csse433")
     q = 'MATCH (u:Test)-[r:tt]->(m:Tests) WHERE u.name="test" RETURN u, type(r), m'
     results = db.query(q, returns=(client.Node, str, client.Node))
     for r in results:
         print("(%s)-[%s]->(%s)" % (r[0]["name"], r[1], r[2]["name"]))
 
 def neo4j_test3():
-    db = GraphDatabase("bolt://433-18.csse.rose-hulman.edu:7687", username="neo4j", password="csse433")
+    db = GraphDatabase("http://433-18.csse.rose-hulman.edu:7687", username="neo4j", password="csse433")
     q = 'MATCH (n) DETACH DELETE n'
     db.query(q, returns=(client.Node, str, client.Node))
 
