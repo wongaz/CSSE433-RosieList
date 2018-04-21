e8ceedf26b0649e67d7962a70608717b5a97d22c
Songyu Wang <wangs4@rose-hulman.edu>
2018-04-20 22:31:44 -0400

    fix

--- neo4j_funcs.py
+++ neo4j_funcs.py
@@ -2,10 +2,7 @@ from neo4jrestclient.client import GraphDatabase
 from neo4jrestclient import client
 def neo4j_test1():
     db = GraphDatabase("http://433-18.csse.rose-hulman.edu:7474", username="neo4j",password="csse433")
-    db.labels.create("Test")
     test = db.nodes.create(name="test")
-
-    db.labels.create("Test2")
     test2 = db.nodes.create(name="test2")
 
     test.relationships.create("tt", test2)
@@ -13,7 +10,7 @@ def neo4j_test1():
 
 def neo4j_test2():
     db = GraphDatabase("http://433-18.csse.rose-hulman.edu:7474", username="neo4j", password="csse433")
-    q = 'MATCH (u:Test)-[r:tt]->(m:Test2) WHERE u.name="test" RETURN u, type(r), m'
+    q = 'MATCH (u)-[r:tt]->(m) WHERE u.name="test" RETURN u, type(r), m'
     results = db.query(q, returns=(client.Node, str, client.Node))
     for r in results:
         print("(%s)-[%s]->(%s)" % (r[0]["name"], r[1], r[2]["name"]))
