6a42a07dd0e9793aed78fb2d253036dfa770c213
Songyu Wang <wangs4@rose-hulman.edu>
2018-05-02 23:23:06 -0400

    fix

--- neo4j_lib.py
+++ neo4j_lib.py
@@ -6,11 +6,13 @@ def add_product(pid,name,desc,tags,price):
     product = "p:Product {{ pid:'{0}',name:'{1}',desc:'{2}',price:'{3}' }}".format(pid,name,desc,price)
     command = "CREATE ({0}) RETURN p.pid".format(product)
     result = run_command(command)
+    product = ""
     for record  in result:
-        return record['p.pid']
-    return 1
+        product =  record['p.pid']
+    if product != pid:
+        return 1
     for tag in tags:
-        c = "MATCH (a:Product),(b:Tag) WHERE a.pid = '{}' AND b.name = '{}' CREATE (a)-[r:Have]->(b) RETURN type(r)".format('hi',tag)
+        c = "MATCH (a:Product),(b:Tag) WHERE a.pid = '{}' AND b.name = '{}' CREATE (a)-[r:Have]->(b) RETURN type(r)".format(product,tag)
         result = run_command(c)
         for record in result:
             if record['type(r)'] != 'Have':
