6878d444e961cbf691faa75b707a54e5ed1f1e99
Alex Wong <wongaz@rose-hulman.edu>
2018-05-04 00:17:47 -0400

    fixed queue service

--- RequeueSystem.py
+++ RequeueSystem.py
@@ -2,6 +2,8 @@ from redis import StrictRedis
 from rq import Queue, get_failed_queue
 import time
 
+MAX_ATTEMPTS = 30
+
 queue_conn = StrictRedis(host='433-19.csse.rose-hulman.edu', port=6379, db=0)
 q = Queue(connection=queue_conn)
 
@@ -17,10 +19,15 @@ while True:
 
         for job_id in failed_queue.job_ids:
             if job_id in dictionary.keys():
-                dictionary2[job_id]=dictionary[job_id]+1
+                dictionary2[job_id] = dictionary[job_id]+1
             else:
                 dictionary2[job_id] = 1
-            failed_queue.requeue(job_id)
+
+            if dictionary2[job_id] <= MAX_ATTEMPTS:
+                failed_queue.requeue(job_id)
+            else:
+                dictionary2.pop(job_id)
+
         f.seek(0)
         for key in dictionary2.keys():
             f.write(key+","+dictionary2.get(key))
@@ -28,4 +35,5 @@ while True:
         f.truncate()
         f.close()
 
+    failed_queue.empty()
     time.sleep(60)
